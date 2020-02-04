502a2ba1e55d5ebe5558b20daf668d1684a027e3
Lakeisha McCree <lmccree215@icloud.com>
2020-02-04 01:11:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+sort_array_asc([25, 7, 1]) #[1,7,25]
+
+def sort_array_desc(array)
+  array.sort do | left, right|
+    right <=> left
+  end
+end
+
+sort_array_desc([25, 7, 14]) #[25, 14, 7]
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
 
